37d7c6a3d05653749146022de9d07e53d9e3f5dc
JOY <joybosu@gmail.com>
2022-02-17 21:09:51 +0530

    Substraction file upload

---
+++ Sub.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Add
+{
+    internal class Sub
+    {
+        public static void SubTwoNum()
+        {
+            Console.WriteLine("Enter fast number a= :");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter secound number b=:");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int c = a - b;
+            Console.WriteLine(c);
+
+        }
+    }
+}
