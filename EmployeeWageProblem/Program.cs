548536f56388b07ed7496262d9308406c9d3ea1b
JOY <joybosu@gmail.com>
2022-02-19 23:00:34 +0530

    Upload new calculate wage Program.cs

---
+++ EmployeeWageProblem/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EmployeeWageProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage problome :");
+
+            EmployeeCheck.EmployeePresentOrNot();
+
+            CalculateEmployeeWage.CalculateWage();
+
+            EmployeeWage.CalEmpWage();
+        }
+    }
+}
